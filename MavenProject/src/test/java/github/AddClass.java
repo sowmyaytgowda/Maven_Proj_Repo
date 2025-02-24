664b9f06399031704d5cbac44a1908d805b86209
SOWMYA Y T <SOWMYA Y T@LAPTOP-FRR416C2.mshome.net>
2025-02-24 19:42:19 +0530

    class added

---
+++ MavenProject/src/test/java/github/AddClass.java
@@ -0,0 +1,12 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AddClass {
+@Test
+public void pushTogithub()
+{
+	Reporter.log("pushing this class to github");
+}
+}
